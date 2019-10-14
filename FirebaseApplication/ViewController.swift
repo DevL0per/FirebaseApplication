33e6d5641c62f790a770f96111ba4b50f9877d88
DevL0per <vazhnik.roma@gmail.com>
2019-10-14 19:23:00 +0300

    Initial Commit

---
+++ FirebaseApplication/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirebaseApplication
+//
+//  Created by Роман Важник on 14/10/2019.
+//  Copyright © 2019 Роман Важник. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
