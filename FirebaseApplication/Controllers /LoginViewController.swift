2207eb0e4c2a5e16e3cfc14c6485f90c6c052d00
DevL0per <vazhnik.roma@gmail.com>
2019-10-16 23:51:02 +0300

    Added a FireBase SDK

--- FirebaseApplication/Controllers /LoginViewController.swift
+++ FirebaseApplication/Controllers /LoginViewController.swift
@@ -1,28 +1,25 @@
 //
-//  RegistrationViewController.swift
+//  LoginViewController.swift
 //  FirebaseApplication
 //
-//  Created by Роман Важник on 14/10/2019.
+//  Created by Роман Важник on 16/10/2019.
 //  Copyright © 2019 Роман Важник. All rights reserved.
 //
 
 import UIKit
+import Firebase
 
-class RegistrationViewController: UIViewController {
-
-    private var backgroundImage: UIImageView! = {
-        let imageView = UIImageView()
-        imageView.image = UIImage(named: "backgroundImage")
-        return imageView
-    }()
+class LoginViewController: UIViewController {
+    
     private var mainStackView: UIStackView!
     
     private var loginTextField = UITextField()
     private var passwordTextField = UITextField()
     private var userMessageLabel: UILabel = {
         let label = UILabel()
-        label.text = "User doesn't exist"
-        label.textColor = .red
+        label.alpha = 0
+        label.textColor = #colorLiteral(red: 0.7450980544, green: 0.2126455816, blue: 0.1617561306, alpha: 1)
+        label.text = " "
         return label
     }()
     
@@ -32,8 +29,12 @@ class RegistrationViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        view.backgroundColor = .white
+        
+        //Configure textFields
         configureTextField(textField: loginTextField)
         configureTextField(textField: passwordTextField)
+        
         configureLoginButton()
         configureRegistrationButton()
         
@@ -41,26 +42,51 @@ class RegistrationViewController: UIViewController {
         
     }
     
-
     //MARK: - Configures
     
     private func configureLoginButton() {
         loginButton = UIButton()
         loginButton.layer.cornerRadius = 3
-        loginButton.backgroundColor = #colorLiteral(red: 0.6935080901, green: 0.6935080901, blue: 0.6935080901, alpha: 0.8040079195)
+        loginButton.backgroundColor = #colorLiteral(red: 0.7450980544, green: 0.2126455816, blue: 0.1617561306, alpha: 1)
         loginButton.titleLabel?.font = UIFont.systemFont(ofSize: 20)
         loginButton.setTitle(" Login ", for: .normal)
         loginButton.addTarget(self, action: #selector(loginButtonTarget), for: .touchUpInside)
     }
     
+    func showWarningLabel(text: String) {
+        userMessageLabel.text = text
+        
+        UIView.animate(withDuration: 3, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseInOut, animations: {
+            [weak self] in
+            self?.userMessageLabel.alpha = 1
+        }) { [weak self] (_) in
+            self?.userMessageLabel.alpha = 0
+        }
+    }
+    
     @objc private func loginButtonTarget() {
-        performSegue(withIdentifier: "toNoteController", sender: nil)
+        guard let email = loginTextField.text, let password = passwordTextField.text,
+            !email.isEmpty, !password.isEmpty else {
+                showWarningLabel(text: "incorrect data")
+                return
+        }
+        
+        Auth.auth().signIn(withEmail: email, password: password) { (user, error) in
+            if error != nil {
+                self.showWarningLabel(text: "authorization error")
+            } else if user != nil {
+                self.performSegue(withIdentifier: "noteSegue", sender: nil)
+                return
+            }
+            self.showWarningLabel(text: "User doesn't exist")
+        }
     }
     
     private func configureRegistrationButton() {
         registrationButton = UIButton()
         registrationButton.backgroundColor = .clear
         registrationButton.setTitle("Registration", for: .normal)
+        registrationButton.setTitleColor(#colorLiteral(red: 0.7450980544, green: 0.2126455816, blue: 0.1617561306, alpha: 1), for: .normal)
         registrationButton.addTarget(self, action: #selector(RegistrationButtonTarget), for: .touchUpInside)
     }
     
@@ -71,22 +97,22 @@ class RegistrationViewController: UIViewController {
     private func configureTextField(textField: UITextField) {
         //textField.layer.borderWidth = 1
         textField.layer.cornerRadius = 3
-        textField.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
-        textField.text = "fdgfdggf"
+        textField.backgroundColor = #colorLiteral(red: 0.937254902, green: 0.937254902, blue: 0.937254902, alpha: 1)
+        textField.leftViewMode = .always
+        
+        let lefrview = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 10))
+        textField.leftView = lefrview
+        
+        if textField == loginTextField {
+            textField.placeholder = "Enter e-mail"
+        } else {
+            textField.placeholder = "Enter password"
+        }
     }
     
     
     //MARK: - Set Constraints
     private func configureStackView() {
-        
-        view.addSubview(backgroundImage)
-        
-        //Set constraints to backgroundImage
-        backgroundImage.translatesAutoresizingMaskIntoConstraints = false
-        backgroundImage.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
-        backgroundImage.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
-        backgroundImage.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
-        backgroundImage.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
         
         //Configure mainStackView
         mainStackView = UIStackView()
@@ -106,11 +132,13 @@ class RegistrationViewController: UIViewController {
         loginTextField.translatesAutoresizingMaskIntoConstraints = false
         loginTextField.rightAnchor.constraint(equalTo: textFieldsStackView.rightAnchor).isActive = true
         loginTextField.leftAnchor.constraint(equalTo: textFieldsStackView.leftAnchor).isActive = true
+        loginTextField.heightAnchor.constraint(equalToConstant: 30).isActive = true
         
         //Set constraints to passwordTextField
         passwordTextField.translatesAutoresizingMaskIntoConstraints = false
         passwordTextField.rightAnchor.constraint(equalTo: textFieldsStackView.rightAnchor).isActive = true
         passwordTextField.leftAnchor.constraint(equalTo: textFieldsStackView.leftAnchor).isActive = true
+        passwordTextField.heightAnchor.constraint(equalToConstant: 30).isActive = true
         
         //Add elements to mainStackView
         mainStackView.addArrangedSubview(userMessageLabel)
@@ -120,9 +148,9 @@ class RegistrationViewController: UIViewController {
         
         //Set Constraints to mainStackView
         mainStackView.translatesAutoresizingMaskIntoConstraints = false
-        mainStackView.leftAnchor.constraint(equalTo: backgroundImage.leftAnchor, constant: 40).isActive = true
-        mainStackView.rightAnchor.constraint(equalTo: backgroundImage.rightAnchor, constant: -40).isActive = true
-        mainStackView.centerYAnchor.constraint(equalTo: backgroundImage.centerYAnchor).isActive = true
+        mainStackView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 40).isActive = true
+        mainStackView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -40).isActive = true
+        mainStackView.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -20).isActive = true
         
         //Set Constraints to textFieldsStackView
         textFieldsStackView.translatesAutoresizingMaskIntoConstraints = false
@@ -141,17 +169,14 @@ class RegistrationViewController: UIViewController {
         view.addSubview(buttonsStackView)
         buttonsStackView.backgroundColor = .red
         buttonsStackView.translatesAutoresizingMaskIntoConstraints = false
-        buttonsStackView.bottomAnchor.constraint(equalTo: backgroundImage.bottomAnchor, constant: -20).isActive = true
-        buttonsStackView.leftAnchor.constraint(equalTo: backgroundImage.leftAnchor, constant: 85).isActive = true
-        buttonsStackView.rightAnchor.constraint(equalTo: backgroundImage.rightAnchor, constant: -85).isActive = true
+        buttonsStackView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -20).isActive = true
+        buttonsStackView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 30).isActive = true
+        buttonsStackView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -30).isActive = true
         
         //Set constraints to loginButton
         loginButton.translatesAutoresizingMaskIntoConstraints = false
         loginButton.leftAnchor.constraint(equalTo: buttonsStackView.leftAnchor).isActive = true
         loginButton.rightAnchor.constraint(equalTo: buttonsStackView.rightAnchor).isActive = true
-        
     }
     
-
 }
-
---
+++ FirebaseApplication/Controllers /RegistrationViewController.swift
@@ -0,0 +1,16 @@
+//
+//  RegistrationViewController.swift
+//  FirebaseApplication
+//
+//  Created by Роман Важник on 14/10/2019.
+//  Copyright © 2019 Роман Важник. All rights reserved.
+//
+
+import UIKit
+
+class RegistrationViewController: UIViewController {
+
+   
+
+}
+
